c9fab4da22e085ee5f4f13b866b051b2512952a0
RegretZZ4 <luigi.cricca@fitstic-edu.com>
2022-03-31 15:04:35 +0200

    setup spadalaser rotation

---
+++ ProgettoTemplate/Assets/Scenes/LuigiScene/RotationLightSaber.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationLightSaber : MonoBehaviour
+{
+    Vector3 angleVelocity;
+
+    bool count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //angleVelocity = new Vector3(90, 0, 0);
+        count = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // RotateSaber();
+        if(!count)
+            StartCoroutine(RotationSaber());
+    }
+
+    void RotateSaber()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(transform.forward * 75f);
+        rigidbody.AddTorque(transform.right * 100f);
+
+        /*
+        Quaternion deltaRotation = Quaternion.Euler(angleVelocity * 1000f);
+        rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
+        */
+    }
+
+    IEnumerator RotationSaber()
+    {
+        count = true;
+
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(transform.up * 500f);
+        rigidbody.AddTorque(transform.right * 5000f);
+
+        yield return new WaitForSeconds(3f);
+
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        this.transform.eulerAngles = new Vector3(90, 0, 0);
+
+        var positionHand = GameObject.Find("RightBaseController");
+
+        this.transform.position = positionHand.transform.position;
+
+        yield return new WaitForSeconds(5f);
+
+        count = false;
+
+    }
+}
