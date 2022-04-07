fe664d33e03a92117a1f6d283285d1581f3ef5fa
RegretZZ4 <luigi.cricca@fitstic-edu.com>
2022-04-07 22:44:38 +0200

    fix rotation
    ora la rotazione avviene quando rilasci la spada laser mentre la stai lanciando in avanti o anche indietro

--- ProgettoTemplate/Assets/Scripts/SpadaLaser/RotationLightSaber.cs
+++ ProgettoTemplate/Assets/Scripts/SpadaLaser/RotationLightSaber.cs
@@ -21,48 +21,17 @@ public class RotationLightSaber : MonoBehaviour
        
     }
 
-    void RotateSaber()
-    {
-        var rigidbody = GetComponent<Rigidbody>();
-
-        rigidbody.AddForce(transform.forward * 75f);
-        rigidbody.AddTorque(transform.right * 100f);
-
-        /*
-        Quaternion deltaRotation = Quaternion.Euler(angleVelocity * 1000f);
-        rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
-        */
-    }
-
-    public IEnumerator RotationSaber()
-    {
-        count = true;
-
-        var rigidbody = GetComponent<Rigidbody>();
-        rigidbody.AddForce(transform.up * 500f);
-        rigidbody.AddTorque(transform.right * 5000f);
-
-        yield return new WaitForSeconds(3f);
-
-        rigidbody.velocity = Vector3.zero;
-        rigidbody.angularVelocity = Vector3.zero;
-        this.transform.eulerAngles = new Vector3(90, 0, 0);
-
-        var positionHand = GameObject.Find("RightBaseController");
-
-        this.transform.position = positionHand.transform.position;
-
-        yield return new WaitForSeconds(5f);
-
-        count = false;
-
-    }
-
     public void GetRotationSaber()
     {
         var rigidbody = GetComponent<Rigidbody>();
-        rigidbody.AddForce(transform.up * 500f);
-        rigidbody.AddTorque(transform.right * 5000f);
+        if (rigidbody.velocity.magnitude > 1)
+        {
+            rigidbody.AddForce(transform.up * 500f);
+            rigidbody.AddTorque(transform.right * 5000f);
+
+            Invoke("ReturnToHand", 3f);
+        }
+
 
 
     }
